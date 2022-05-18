9d51d36e58da3c38e3ff2f863a2e638bbed81c0c
LFhan <hancx18@126.com>
2022-05-18 14:08:50 +0800

    test

--- EmployeeController.java
+++ EmployeeController.java
@@ -1,5 +1,7 @@
 public class EmployeeController {
            public Employee queryEmployee（）{
-                     return “empList”:
+                     return “empList”;
            }
+           public int queryEmployee（）{
+                     return “status”；
 }
